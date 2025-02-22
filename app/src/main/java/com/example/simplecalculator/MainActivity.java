0ac89867c2764e34a13f3e7dce590509225016d2
danaB <danabarnea2007@gmail.com>
2025-02-22 17:48:09 +0200

    added logs

--- app/src/main/java/com/example/simplecalculator/MainActivity.java
+++ app/src/main/java/com/example/simplecalculator/MainActivity.java
@@ -16,58 +16,70 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    public class MainActivity extends AppCompatActivity {
+        FirebaseAuth auth;
+        GoogleSignInClient googleSignInClient;
+        ShapeableImageView imageView;
+        TextView name, mail;
+
+        private final ActivityResultLauncher<Intent> activityResultLauncher = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                new ActivityResultCallback<ActivityResult>() {
+                    @Override
+                    public void onActivityResult(ActivityResult result) {
+                        if (result.getResultCode() == RESULT_OK) {
+                            Task<GoogleSignInAccount> accountTask = GoogleSignIn.getSignedInAccountFromIntent(result.getData());
+                            try {
+                                GoogleSignInAccount signInAccount = accountTask.getResult(ApiException.class);
+                                AuthCredential authCredential = GoogleAuthProvider.getCredential(signInAccount.getIdToken(), null);
+                                auth.signInWithCredential(authCredential);
+                            } catch (ApiException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }
+                }
+        );
+        @Override
+        public void onComplete(@NonNull Task<AuthResult> task) {
+            if (task.isSuccessful()) {
+                auth = FirebaseAuth.getInstance();
+                Glide.with(MainActivity.this).load(Objects.requireNonNull(auth.getCurrentUser()).getPhotoUrl()).into(imageView);
+                name.setText(auth.getCurrentUser().getDisplayName());
+                mail.setText(auth.getCurrentUser().getEmail());
+                Toast.makeText(MainActivity.this, "Signed in successfully", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(MainActivity.this, "Failed to sign in: " + task.getException(), Toast.LENGTH_SHORT).show();
+            }
+        }
+    });
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        FirebaseApp.initializeApp(context: this);
+        imageView = findViewById(R.id.profileImage);
+        name = findViewById(R.id.nameTV);
+        mail = findViewById(R.id.mailTV);
+
+        GoogleSignInOptions options = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.client_id))
+                .requestEmail()
+                .build();
+
+        googleSignInClient = GoogleSignIn.getClient(@NonNull MainActivity.this, options);
+
+        auth = FirebaseAuth.getInstance();
+
+        SignInButton signInButton = findViewById(R.id.signIn);
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = googleSignInClient.getSignInIntent();
+                activityResultLauncher.launch(intent);
+            }
         });
-    }
-
-    @SuppressLint("SetTextI18n")
-    public void onBtnClicked(View view) {
-        // Get view handle using the view id
-        EditText et1 = findViewById(R.id.Num1);
-
-        // Get the view’s text
-        String et1Text = et1.getText().toString();
-
-        // Cast the String into Integer
-        Integer num1 = Integer.valueOf(et1Text);
-
-        // Get view handle using the view id
-        EditText et2 = findViewById(R.id.Num2);
-
-        // Get the view’s text
-        String et2Text = et2.getText().toString();
-
-        // Cast the String into Integer
-        Integer num2 = Integer.valueOf(et2Text);
-        Log.e("num1=", num1.toString());
-        Log.e("num2=", num2.toString());
-
-         Integer result = null;
-        if (view.getId() == R.id.btnPlus) {
-            result = num1 + num2;
-            Log.e("result=",result.toString());
-
-        }
-        if (view.getId() == R.id.btnMinus)
-            result = num1 - num2;
-        if (view.getId() == R.id.btnMult)
-            result = num1 * num2;
-        if (view.getId() == R.id.btnDiv)
-            result = num1 / num2;
-        if (result != null) {
-            Log.e("show result=", result.toString());
-
-            TextView tvRes = findViewById(R.id.tvResult);
-            tvRes.setText(result.toString());
-        }
-
     }
 }
